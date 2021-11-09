152f80f91d67edec68e9af81a294a2ebd21d49f4
Flexberry-man <mail@flexberry.net>
2021-11-09 09:28:06 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,25 +22,25 @@ export default Controller.extend({
             link: 'i-i-s-s-m-customer-request-l',
             caption: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-customer-request-l.caption'),
             title: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-customer-request-l.title'),
-            icon: 'briefcase',
+            icon: 'list',
             children: null
           }, {
             link: 'i-i-s-s-m-ticket-l',
             caption: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-ticket-l.caption'),
             title: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-ticket-l.title'),
-            icon: 'building',
+            icon: 'folder',
             children: null
           }, {
             link: 'i-i-s-s-m-event-l',
             caption: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-event-l.caption'),
             title: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-event-l.title'),
-            icon: 'file',
+            icon: 'suitcase',
             children: null
           }, {
             link: 'i-i-s-s-m-feedback-l',
             caption: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-feedback-l.caption'),
             title: i18n.t('forms.application.sitemap.events-and-requests.i-i-s-s-m-feedback-l.title'),
-            icon: 'book',
+            icon: 'list',
             children: null
           }]
         }, {
@@ -52,7 +52,7 @@ export default Controller.extend({
             link: 'i-i-s-s-m-customer-l',
             caption: i18n.t('forms.application.sitemap.profile.i-i-s-s-m-customer-l.caption'),
             title: i18n.t('forms.application.sitemap.profile.i-i-s-s-m-customer-l.title'),
-            icon: 'folder',
+            icon: 'edit',
             children: null
           }]
         }
